4fda05285cfcb13b6337716838776c6ca6d64ea4
njogued <njogued@gmail.com>
2023-07-06 23:42:14 +0300

    Define variables with types

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Annotate variables to values"""
+a = int
+pi = float
+i_understand_annotations = bool
+school = str
+
+a = 1
+pi = 3.14
+i_understand_annotations = True
+school = "Holberton"
