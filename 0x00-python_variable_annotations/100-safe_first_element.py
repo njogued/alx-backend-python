8933e9180c2a790ba60df56f722601264b8d3f24
njogued <njogued@gmail.com>
2023-07-07 00:54:25 +0300

    Any??

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""add correct annotations"""
+from typing import Sequence
+from typing import Any
+from typing import Union
+
+lstype = Sequence[Any]
+rtype = Union[Any, None]
+
+
+def safe_first_element(lst: lstype) -> rtype:
+    if lst:
+        return lst[0]
+    else:
+        return None
