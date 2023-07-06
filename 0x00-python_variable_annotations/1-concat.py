f20a83d85f4439e2bf2deec5945367cab480a9cb
njogued <njogued@gmail.com>
2023-07-06 23:33:15 +0300

    Floor value

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -3,4 +3,5 @@
 
 
 def concat(str1: str, str2: str) -> str:
+    """Function to concatenate two strings"""
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Function that takes in a float and returns floor value"""
+
+
+def floor(n: float) -> int:
+    """A function that returns the floor"""
+    return int(n)
