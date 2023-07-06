d584cbb25430b635e920a2067b521c1c44959298
njogued <njogued@gmail.com>
2023-07-07 00:34:12 +0300

    Return a callable function

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type-annotated function that returns Callable function"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """return a function given a float"""
+    def multiplez(x):
+        return x * x
+    return multiplez
