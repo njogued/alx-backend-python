25c04fdaf45ef98d69aca9f310d3a23c9aa5d689
njogued <njogued@gmail.com>
2023-07-12 02:19:19 +0300

    async comprehension

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""async_generator coroutine that yields a random module"""
+import random
+from typing import Generator
+import asyncio
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """Loop in a range of 10 and then return a random value"""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random() * 10
