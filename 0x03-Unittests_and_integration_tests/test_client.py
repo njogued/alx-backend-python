cd082fbdd242e6b16fd28732ede486159ecd6f83
njogued <njogued@gmail.com>
2023-08-05 02:35:54 +0300

    Test has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -25,3 +25,13 @@ class TestGithubOrgClient(unittest.TestCase):
             mock_obj.return_value = repo
             self.assertEqual(GithubOrgClient('google').
                              _public_repos_url, repo['repos_url'])
+
+    @parameterized.expand([
+        ({'license': {'key': "bsd-3-clause"}}, "bsd-3-clause", True),
+        ({'license': {'key': "bsl-1.0"}}, "bsd-3-clause", False),
+    ])
+    def test_has_license(self, repo, key, expected):
+        """Tests the has license method."""
+        gh_org_client = GithubOrgClient("google")
+        client_has_licence = gh_org_client.has_license(repo, key)
+        self.assertEqual(client_has_licence, expected)
