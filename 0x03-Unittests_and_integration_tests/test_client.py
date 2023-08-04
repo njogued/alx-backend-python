ab50f8f4672835398eadd0ccb8ff532fcec6258e
njogued <njogued@gmail.com>
2023-08-05 02:42:50 +0300

    Integration test of client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Module to test the client module in client.py"""
 import unittest
-from unittest.mock import patch, PropertyMock
+from unittest.mock import patch, PropertyMock, Mock
 from parameterized import parameterized
+from requests import HTTPError
 GithubOrgClient = __import__('client').GithubOrgClient
 
 
@@ -35,3 +36,44 @@ class TestGithubOrgClient(unittest.TestCase):
         gh_org_client = GithubOrgClient("google")
         client_has_licence = gh_org_client.has_license(repo, key)
         self.assertEqual(client_has_licence, expected)
+
+
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """an integration testing class for end to end testing"""
+    @classmethod
+    def setUpClass(cls) -> None:
+        """Function that runs before any other class and sets up context
+        """
+        route_payload = {
+            'https://api.github.com/orgs/google': cls.org_payload,
+            'https://api.github.com/orgs/google/repos': cls.repos_payload,
+        }
+
+        def get_payload(url):
+            if url in route_payload:
+                return Mock(**{'json.return_value': route_payload[url]})
+            return HTTPError
+
+        cls.get_patcher = patch("requests.get", side_effect=get_payload)
+        cls.get_patcher.start()
+
+    def test_public_repos(self) -> None:
+        """method to test GithubOrgClient.public_repos
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(),
+            self.expected_repos,
+        )
+
+    def test_public_repos_with_license(self) -> None:
+        """test the public_repos with the argument license=apache-2.0
+        """
+        self.assertEqual(
+            GithubOrgClient("google").public_repos(license="apache-2.0"),
+            self.apache2_repos,
+        )
+
+    @classmethod
+    def tearDownClass(cls) -> None:
+        """The teardownclass runs after all methods have run"""
+        cls.get_patcher.stop()
